9887ed0ba3c2339777d736785e64c48cde2e6918
rivo pelu <yannai.ming@falkcia.com>
2024-01-26 19:22:34 +0800

    FEAT:fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import { Container } from "./components/Container.tsx";
 
 function App() {
   return (
-    <div className={"max-w-md mx-auto h-full "}>
-      <Container>
-        <div className={"flex flex-col items-center justify-center"}>
-          <h1 className={"font-1 w-full  text-4xl text-center text-amber-600"}>
-            Happy Graduation
-          </h1>
+    <div className={"max-w-md mx-auto h-[100dvh] "}>
+      <div className={"py-10 flex items-center justify-center h-full "}>
+        <Container>
+          <div className={"flex flex-col items-center justify-center gap-10"}>
+            <h1
+              className={"font-1 w-full  text-4xl text-center text-amber-600"}
+            >
+              Happy Graduation
+            </h1>
 
-          <img className={"w-[100%]"} src={"/cax.jpeg"} alt={"cax"} />
-        </div>
-      </Container>
+            <img
+              className={"w-[80%] ring ring-white rounded-md"}
+              src={"/cax.png"}
+              alt={"cax"}
+            />
+            <div className={"font-bold text-center"}>
+              Ns. Tessalonika M. Laluraga S.Kep
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
