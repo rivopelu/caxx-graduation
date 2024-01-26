e95848e2182904e1bf33bacf6f08aa89c218a281
rivo pelu <yannai.ming@falkcia.com>
2024-01-26 19:42:15 +0800

    FEAT:fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,39 @@ import { Container } from "./components/Container.tsx";
 
 function App() {
   return (
-    <div className={"max-w-md mx-auto h-[100dvh] "}>
-      <div className={"py-10 flex items-center justify-center h-full "}>
-        <Container>
-          <div className={"flex flex-col items-center justify-center gap-10"}>
-            <h1
-              className={"font-1 w-full  text-4xl text-center text-amber-600"}
-            >
-              Happy Graduation
-            </h1>
+    <div className={"h-full w-full relative"}>
+      <div className={"max-w-md mx-auto h-[100dvh] z-40  relative"}>
+        <div className={"py-10 flex items-center justify-center h-full "}>
+          <Container>
+            <div className={"flex flex-col items-center justify-center gap-10"}>
+              <h1
+                className={"font-1 w-full  text-4xl text-center text-amber-600"}
+              >
+                Happy Graduation
+              </h1>
 
-            <img
-              className={"w-[80%] ring ring-white rounded-md"}
-              src={"/cax.png"}
-              alt={"cax"}
-            />
-            <div className={"font-bold text-center"}>
-              Ns. Tessalonika M. Laluraga S.Kep
+              <img
+                className={"w-[80%]  rounded-md"}
+                src={"/cax.png"}
+                alt={"cax"}
+              />
+              <div className={"font-bold text-xl text-center"}>
+                Ns. Tessalonika M. Laluraga S.Kep
+              </div>
             </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
       </div>
+      <div
+        className={
+          "h-96 w-96 bg-primary-1/30 rounded-full absolute top-[-20%] left-[-30%] z-10"
+        }
+      />
+      <div
+        className={
+          "h-56 w-56 bg-primary-1/20 rounded-full absolute bottom-[3%] right-[-20%] z-10"
+        }
+      />
     </div>
   );
 }
