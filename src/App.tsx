d35364ff614b270c885b0d3a0ff13cc827e22158
rivo pelu <yannai.ming@falkcia.com>
2024-01-28 00:30:38 +0800

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Container } from "./components/Container.tsx";
 import { ImageContainer } from "./components/ImageContainer.tsx";
+import { useEffect } from "react";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import AOS from "aos";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+    });
+  }, []);
   return (
     <div className={"h-full w-full relative"}>
       <div className={"max-w-md mx-auto h-[100dvh] z-40  relative"}>
@@ -9,6 +17,7 @@ function App() {
           <Container>
             <div className={"flex flex-col items-center justify-center gap-10"}>
               <h1
+                data-aos="fade-up"
                 className={
                   "font-1 w-full  text-3xl  text-center text-primary-dark"
                 }
@@ -18,14 +27,22 @@ function App() {
 
               <ImageContainer />
               <div className={"text-center"}>
-                <div className={"font-bold text-xl text-center"}>
+                <div
+                  data-aos-delay="1000"
+                  className={"font-bold text-xl text-center"}
+                  data-aos="zoom-in"
+                >
                   Ns. Tessalonika M. Laluraga S.Kep
                 </div>
-                <div>
+                <div data-aos="zoom-out-up" data-aos-delay="1300">
                   wish you all the best in your new chapter. May you have a
                   successful and fulfilling career.
                 </div>
-                <div className={"text-sm mt-3 text-slate-900/40"}>
+                <div
+                  data-aos-delay="2600"
+                  data-aos="fade-up"
+                  className={"text-sm mt-3 text-slate-900/40"}
+                >
                   from your bear🧸❤️
                 </div>
               </div>
--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,21 +1,43 @@
 export function ImageContainer() {
   return (
     <div
-      className={"bg-red-500 h-[400px] w-[80%] rounded-xl p-3"}
+      data-aos="zoom-in"
+      data-aos-offset="500"
+      data-aos-duration="1000"
+      data-aos-delay="200"
+      className={"bg-red-500 h-[400px] w-[80%] rounded-xl p-3 overflow-hidden"}
       style={{ background: "#BF8A75" }}
     >
       <div
+        data-aos="fade-up"
+        data-aos-delay="400"
         className={
           "bg-green-800 h-full rounded-md  flex flex-col justify-between"
         }
         style={{ background: "#D0ABA5" }}
       >
         <div className={"flex items-center gap-2 m-3"}>
-          <div className={"h-3 w-3 bg-[#FF5F57] rounded-full"}></div>
-          <div className={"h-3 w-3 bg-[#FEBC2E] rounded-full"}></div>
-          <div className={"h-3 w-3 bg-[#28C840] rounded-full"}></div>
+          <div
+            data-aos="fade-down"
+            data-aos-delay="1000"
+            className={"h-3 w-3 bg-[#FF5F57] rounded-full"}
+          ></div>
+          <div
+            data-aos="fade-down"
+            data-aos-delay="1200"
+            className={"h-3 w-3 bg-[#FEBC2E] rounded-full"}
+          ></div>
+          <div
+            data-aos="fade-down"
+            data-aos-delay="1400"
+            className={"h-3 w-3 bg-[#28C840] rounded-full"}
+          ></div>
         </div>
-        <div className={"translate-x-2 flex justify-center"}>
+        <div
+          data-aos="zoom-in-up"
+          data-aos-delay="700"
+          className={"translate-x-2 flex justify-center"}
+        >
           <img className={"h-[340px]"} src={"/img.png"} alt={"cax"} />
         </div>
       </div>
